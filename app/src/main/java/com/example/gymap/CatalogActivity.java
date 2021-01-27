7c1d45b7765c71d11d55c17f1b7b6342a402e8ae
Simaha <simahaebrima@gmail.com>
2021-01-27 07:10:35 +0000

    menu option added to catalog activity

--- app/src/main/java/com/example/gymap/CatalogActivity.java
+++ app/src/main/java/com/example/gymap/CatalogActivity.java
@@ -1,9 +1,12 @@
 package com.example.gymap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -26,4 +29,24 @@ public class CatalogActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()){
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                return true;
+            case R.id.action_delete_all_entries:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
