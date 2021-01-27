7ef140a5e68baf47e6eb121af38118af0fa3cb02
Simaha <simahaebrima@gmail.com>
2021-01-27 05:57:08 +0000

    EditorActivity added

--- app/src/main/java/com/example/gymap/CatalogActivity.java
+++ app/src/main/java/com/example/gymap/CatalogActivity.java
@@ -2,7 +2,11 @@ package com.example.gymap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        //Setup FAB to open EditorActivity
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
