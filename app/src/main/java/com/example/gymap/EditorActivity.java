036ab4c1f2d93d5321f7b3ad87def929659c4077
Simaha <simahaebrima@gmail.com>
2021-02-17 18:49:47 +0000

    add member from Edit activity

--- app/src/main/java/com/example/gymap/EditorActivity.java
+++ app/src/main/java/com/example/gymap/EditorActivity.java
@@ -1,5 +1,7 @@
 package com.example.gymap;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -9,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,12 +20,16 @@ import androidx.core.app.NavUtils;
 
 import com.example.gymap.data.GymContract;
 import com.example.gymap.data.GymContract.GymEntry;
+import com.example.gymap.data.GymDbHelper;
 
 import org.w3c.dom.Text;
 
 public class EditorActivity extends AppCompatActivity {
     private EditText mNameEditText;
+    private EditText mAgeEditText;
     private Spinner mGenderSpinner;
+    private EditText mWeightEditText;
+
 
     private int mGender = GymEntry.GENDER_MALE;
 
@@ -32,7 +39,9 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
 
         mNameEditText = findViewById(R.id.edit_name);
+        mAgeEditText = findViewById(R.id.edit_age);
         mGenderSpinner = findViewById(R.id.spinner_gender);
+        mWeightEditText = findViewById(R.id.edit_weight);
 
         setupSpinner();
     }
@@ -74,7 +83,39 @@ public class EditorActivity extends AppCompatActivity {
      * Get user input from editor and save new pet into database.
      */
     private void insertMember(){
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        String nameString = mNameEditText.getText().toString().trim();
+        String ageString = mAgeEditText.getText().toString().trim();
+        String weightString = mWeightEditText.getText().toString().trim();
+        int age = Integer.parseInt(ageString);
+        int weight = Integer.parseInt(weightString);
 
+        //Create database helper
+        GymDbHelper mDbHelper = new GymDbHelper(this);
+
+        //Gets the database in writable mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and member attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(GymEntry.COLUMN_NAME, nameString);
+        values.put(GymEntry.COLUMN_AGE, age);
+        values.put(GymEntry.COLUMN_GENDER, mGender);
+        values.put(GymEntry.COLUMN_WEIGHT, weight);
+
+        // Insert a new row for pet in the database, returning the ID of that new row.
+        long newRowId = db.insert(GymEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1){
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving New Member", Toast.LENGTH_SHORT).show();
+        }else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "New Member Saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -88,6 +129,10 @@ public class EditorActivity extends AppCompatActivity {
         // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()){
             case R.id.action_save:
+                //Save a new member to database
+                insertMember();
+                //Exit Activity
+                finish();
                 return true;
             case R.id.action_delete:
                 return true;
